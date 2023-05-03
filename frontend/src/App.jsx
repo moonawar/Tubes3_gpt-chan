e99af1f65f5dcf6ffabe4ae15e52253f0e9e3d1a
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-05-03 21:45:56 +0700

    feat: dialog boxes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import styles from './App.module.css';
-import {createSignal} from "solid-js";
+import {createSignal, For} from "solid-js";
 
 
 function MessageBox() {
@@ -60,13 +60,71 @@ function MessageBox() {
     );
 }
 
+function DialogBox(props) {
+    return (
+        <div style={{
+            width: "100%",
+        }}>
+            <div style={{
+                "font-size": "1rem",
+                "line-height": "1.5rem",
+                display: "flex",
+                margin: "auto",
+                gap: "1.5rem",
+                "max-width": "48rem",
+                "padding": "1.5rem 0",
+            }}>
+                <div>{props.speaker}</div>
+                <div style={{
+                    position: "relative",
+                    width: "calc(100% - 100px)",
+                }}>
+                    <div style={{
+                        "min-height": "20px",
+                        "white-space": "pre-wrap",
+                        "word-wrap": "break-word",
+                    }}>
+                        {props.text}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function App() {
     return (
         <div className={styles.App}>
             <div className={styles.left}></div>
             <div className={styles.right}>
                 <main className={styles.main}>
-                    <div className={styles.dialogsContainer}></div>
+                    <div className={styles.dialogsContainer}>
+                        <div style={{
+                            position: "relative",
+                            height: "100%",
+                        }}>
+                            <div style={{
+                                height: "100%",
+                                width: "100%",
+                                "overflow-y": "auto",
+                            }}>
+                                <div style={{
+                                    display: "flex",
+                                    "flex-direction": "column",
+                                    "align-items": "center",
+                                }}>
+                                    <For each={[...Array(10).keys()]}>{(x, _) =>
+                                        <DialogBox speaker="You" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eveniet nihil nostrum quae quis ratione, tempora vero? A aliquam, consequatur error fugit maxime porro veniam! Ad alias assumenda at eos, eveniet ex incidunt ipsum laborum maiores modi nemo nobis provident quis, quos repellendus totam ullam? Aliquam aperiam ducimus facere in minima necessitatibus nemo neque non omnis quas, quibusdam, reiciendis sed sint tempora veritatis. Aliquam animi assumenda cumque cupiditate excepturi facilis fugit hic in libero minus nam non nulla officia officiis possimus quae quas quo quos recusandae reiciendis sint soluta temporibus, unde ut vel vitae voluptatibus. Aliquid consequuntur dolores laboriosam nihil."/>
+                                    }</For>
+                                    <div style={{
+                                        width: "100%",
+                                        height: "12rem",
+                                        "flex-shrink": "0",
+                                    }}></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className={styles.formSection}>
                         <MessageBox/>
                         <div style={{
@@ -77,7 +135,7 @@ function App() {
                             "padding-right": "1rem",
                             "font-size": "0.75rem",
                             "line-height": "1rem",
-                            "text-align": "center"
+                            "text-align": "center",
                         }}>
                             Created by: Addin Munawwar, Moch. Sofyan Firdaus, and Ezra M. C. M. H.
                         </div>
