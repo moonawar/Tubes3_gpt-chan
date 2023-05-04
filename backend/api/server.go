5300d81ecaf93f2d2d57243821b5ffd99d94009c
Addin Munawwar <moonawar@protonmail.com>
2023-05-04 20:45:01 +0700

    cors

--- backend/api/server.go
+++ backend/api/server.go
@@ -17,6 +17,14 @@ func NewServer(query *db.Queries) *Server {
 		router: gin.Default(),
 	}
 
+	server.router.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Next()
+	})
+
 	server.router.POST("/user", server.AddUser)
 	server.router.POST("/chat", server.CreateChat)
 	server.router.GET("/chat/:username", server.GetUserChat)
