cb22f3a9740862a5386f7e58cf3b681eb8c7dfa5
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-05-04 23:06:29 +0700

    feat: client-server communication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,98 +1,10 @@
 import styles from './App.module.css';
 import {createSignal, For} from "solid-js";
-
-
-function MessageBox() {
-    const [msg, setMsg] = createSignal("");
-    const sendMsgEnter = ({target}) => {
-        if (msg().length > 0) {
-            target.style.background = "#11111180";
-            target.style.cursor = "pointer";
-        }
-    };
-    const questionKeyDown = event => {
-        if (event.key === "Enter" && !event.shiftKey) {
-            event.preventDefault();
-        }
-    };
-    const onMsgInput = ({target}) => {
-        setMsg(target.value);
-        target.style.height = 0;
-        target.style.height = Math.max(target.scrollHeight, 24) + 'px';
-        if (target.value.length > 0) {
-            document.getElementById("send-msg-wrapper").style.color = "#DDD";
-        } else {
-            document.getElementById("send-msg-wrapper").style.color = "#AAA";
-        }
-    };
-    const sendMsgLeave = ({target}) => {
-        target.style.background = "transparent";
-        target.style.cursor = "default";
-    };
-
-    return (
-        <form className={styles.msgWrapper}>
-            <div className={styles.textAreaWrapper}>
-                <textarea
-                    autoFocus={true}
-                    className={styles.msg}
-                    id="question"
-                    name="question"
-                    tabIndex={0}
-                    value={msg()}
-                    placeholder="Send a message."
-                    onInput={onMsgInput}
-                    rows={1}
-                    onKeyDown={questionKeyDown}/>
-                <div
-                    id="send-msg-wrapper"
-                    className={styles.sendMsg}
-                    onMouseEnter={sendMsgEnter}
-                    onMouseLeave={sendMsgLeave}
-                    role="button">
-                    <span id="send-msg"
-                          className="material-symbols-outlined">
-                        send
-                    </span>
-                </div>
-            </div>
-        </form>
-    );
-}
-
-function DialogBox(props) {
-    return (
-        <div style={{
-            width: "100%",
-        }}>
-            <div style={{
-                "font-size": "1rem",
-                "line-height": "1.5rem",
-                display: "flex",
-                margin: "auto",
-                gap: "1.5rem",
-                "max-width": "48rem",
-                "padding": "1.5rem 0",
-            }}>
-                <div>{props.speaker}</div>
-                <div style={{
-                    position: "relative",
-                    width: "calc(100% - 100px)",
-                }}>
-                    <div style={{
-                        "min-height": "20px",
-                        "white-space": "pre-wrap",
-                        "word-wrap": "break-word",
-                    }}>
-                        {props.text}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+import {DialogBox} from "./components/DialogBox";
+import {MessageBox} from "./components/MessageBox";
 
 function App() {
+    const [dialogList, setDialogs] = createSignal(dialogs);
     return (
         <div className={styles.App}>
             <div className={styles.left}></div>
@@ -113,8 +25,10 @@ function App() {
                                     "flex-direction": "column",
                                     "align-items": "center",
                                 }}>
-                                    <For each={[...Array(10).keys()]}>{(x, _) =>
-                                        <DialogBox speaker="You" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate eveniet nihil nostrum quae quis ratione, tempora vero? A aliquam, consequatur error fugit maxime porro veniam! Ad alias assumenda at eos, eveniet ex incidunt ipsum laborum maiores modi nemo nobis provident quis, quos repellendus totam ullam? Aliquam aperiam ducimus facere in minima necessitatibus nemo neque non omnis quas, quibusdam, reiciendis sed sint tempora veritatis. Aliquam animi assumenda cumque cupiditate excepturi facilis fugit hic in libero minus nam non nulla officia officiis possimus quae quas quo quos recusandae reiciendis sint soluta temporibus, unde ut vel vitae voluptatibus. Aliquid consequuntur dolores laboriosam nihil."/>
+                                    <For each={dialogList()}>{(dialog, idx) =>
+                                        <DialogBox speaker={idx() % 2 === 0 ? `${USERNAME}` : "GPT-chan"} background={idx() % 2 === 0 ? "" : "#313244"}>
+                                            {dialog}
+                                        </DialogBox>
                                     }</For>
                                     <div style={{
                                         width: "100%",
@@ -126,7 +40,7 @@ function App() {
                         </div>
                     </div>
                     <div className={styles.formSection}>
-                        <MessageBox/>
+                        <MessageBox dialogs={dialogList} setDialogs={setDialogs}/>
                         <div style={{
                             color: "white",
                             "padding-bottom": "1.75rem",
