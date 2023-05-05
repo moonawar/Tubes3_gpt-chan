af5f53f66b462bf749f2cd14869a4ae89bc99da2
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-05-05 17:03:52 +0700

    feat: move global declarations to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,93 @@
 import styles from './App.module.css';
-import {For} from "solid-js";
+import {createSignal, For} from "solid-js";
 import {DialogBox} from "./components/DialogBox";
 import {MessageBox} from "./components/MessageBox";
-import {
-    chats,
-    current_algorithm,
-    current_chat_id,
-    dialogs,
-    loadData,
-    SERVER_URL, setCurrentChatId,
-    setDialogs,
-    USERNAME
-} from "./globals";
 import {HistoryBox} from "./components/HistoryBox";
+
+const USERNAME = "JoJo";
+const SERVER_URL = `http://${import.meta.env.VITE_SERVER_ADDRESS}`;
+const [dialogs, setDialogs] = createSignal([]);
+const [chats, setChats] = createSignal([]);
+const [current_chat_id, setCurrentChatId] = createSignal(1);
+const [current_algorithm, setCurrentAlgorithm] = createSignal("kmp");
+
+function logInOrSignUp() {
+    fetch(`${SERVER_URL}/user`, {
+        method: "POST",
+        body: JSON.stringify({username: USERNAME}),
+    }).then(response => {
+        if (!response.ok) {
+            return Promise.reject(response);
+        }
+        return response.json();
+    }).then(_ => {
+        console.log(`Sign Up: ${USERNAME}`)
+    }).catch(_ => {
+        console.log(`Sign In: ${USERNAME}`)
+    });
+}
+
+function loadDialogs() {
+    fetch(
+        `${SERVER_URL}/message?chat_id=${current_chat_id()}&limit=100&page=1`
+    ).then(response => {
+        if (!response.ok) {
+            return Promise.reject(response);
+        }
+        return response.json();
+    }).then(async data => {
+        let arr = await data;
+        if (arr !== null) {
+            for (let obj of Object.values(arr)) {
+                setDialogs(prev => prev.concat(obj["question"], obj["answer"]));
+            }
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+function createChat() {
+    fetch(`${SERVER_URL}/chat`, {
+        method: "POST",
+        body: JSON.stringify({
+            username: USERNAME
+        }),
+    }).then(response => {
+        if (!response.ok) {
+            return Promise.reject(response);
+        }
+        return response.json();
+    }).then(async data => {
+        let json = await data;
+        setCurrentChatId(json["chat_id"]);
+    });
+}
+
+function loadChats() {
+    fetch(`${SERVER_URL}/chat/${USERNAME}`).then(response => {
+        if (!response.ok) {
+            return Promise.reject(response);
+        }
+        return response.json();
+    }).then(async data => {
+        if (data !== null) {
+            let result = await data;
+            setCurrentChatId(result[0]);
+            setChats(prev => prev.concat(result));
+            loadDialogs(current_chat_id());
+        } else {
+            createChat();
+        }
+    }).catch(error => {
+        console.log(error)
+    });
+}
+
+export function loadData() {
+    logInOrSignUp();
+    loadChats();
+}
 
 function sendMsg(msg) {
     console.log(`Send message: ${msg}`)
